aa03878b53ac2231f5c6692127792d1a8e64725d
Ce1ling <2134045201@qq.com>
2022-11-26 10:02:08 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <HelloWorld msg="Violet UI" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>()
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  color: blueviolet;
+}
+</style>
