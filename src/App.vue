684559f943688e621b649b8658ad63e3c6c26058
Ce1ling <2134045201@qq.com>
2022-11-28 18:30:34 +0800

    增加：添加返回顶部功能

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,41 @@
 <script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue';
+
+
+const showGoTop = ref(false)
+
+const handleScroll = () => {
+  const halfHeight = window.innerHeight * 0.5
+  if (window.scrollY >= halfHeight) { showGoTop.value = true } 
+  else { showGoTop.value = false }
+}
+const goTop = () => window.scrollTo({ top: 0 })
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
 </script>
 
 <template>
   <router-view />
+  <div class="go-top-wrap" v-show="showGoTop" @click="goTop"> TOP </div>
 </template>
 
-<style scoped>
+<style lang="scss">
+.go-top-wrap {
+  user-select: none;
+  position: fixed;
+  right: 100px;
+  bottom: 100px;
+  border: 1px solid #ddd;
+  padding: 10px;
+  background-color: #fff;
+  &:hover {
+    cursor: pointer;
+    background-color: #ddd;
+  }
+}
 </style>
