76717e5612da3cd636aa96c0fd870cd45363110a
Ce1ling <2134045201@qq.com>
2022-12-03 23:28:47 +0800

    增加：新增亿些 icon

--- src/views/Icon/index.vue
+++ src/views/Icon/index.vue
@@ -7,7 +7,28 @@
     <vi-icon name="search" size="200px" color="blueviolet" />
     <vi-icon name="loading" size="200px" color="blueviolet" />
     <vi-icon name="close" size="200px" color="blueviolet" />
+    <vi-icon name="check" size="200px" color="blueviolet" />
     <vi-icon name="top" size="200px" color="blueviolet" />
+    <vi-icon name="check" size="200px" color="blueviolet" />
+    <vi-icon name="delete" size="200px" color="blueviolet" />
+    <vi-icon name="edit" size="200px" color="blueviolet" />
+    <vi-icon name="envelop" size="200px" color="blueviolet" />
+    <vi-icon name="female" size="200px" color="blueviolet" />
+    <vi-icon name="male" size="200px" color="blueviolet" />
+    <vi-icon name="fullscreen" size="200px" color="blueviolet" />
+    <vi-icon name="home" size="200px" color="blueviolet" />
+    <vi-icon name="link" size="200px" color="blueviolet" />
+    <vi-icon name="linux" size="200px" color="blueviolet" />
+    <vi-icon name="mac" size="200px" color="blueviolet" />
+    <vi-icon name="windows" size="200px" color="blueviolet" />
+    <vi-icon name="menu" size="200px" color="blueviolet" />
+    <vi-icon name="minus" size="200px" color="blueviolet" />
+    <vi-icon name="plus" size="200px" color="blueviolet" />
+    <vi-icon name="setting" size="200px" color="blueviolet" />
+    <vi-icon name="user" size="200px" color="blueviolet" />
+    <vi-icon name="warning" size="200px" color="blueviolet" />
+    <vi-icon name="zoomin" size="200px" color="blueviolet" />
+    <vi-icon name="zoomout" size="200px" color="blueviolet" />
   </div>
 </template>
 
