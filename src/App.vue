2e1002088ff57afc01618bad85f3cd32b445792a
Ce1ling <2134045201@qq.com>
2022-12-04 13:48:10 +0800

    完善：将回到顶部功能从 App 移动到 layout 中

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,9 @@
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue';
-
-
-const showGoTop = ref(false)
-
-const handleScroll = () => {
-  const halfHeight = window.innerHeight * 0.5
-  if (window.scrollY >= halfHeight) { showGoTop.value = true } 
-  else { showGoTop.value = false }
-}
-const goTop = () => window.scrollTo({ top: 0 })
-
-onMounted(() => {
-  window.addEventListener('scroll', handleScroll)
-})
-onUnmounted(() => {
-  window.removeEventListener('scroll', handleScroll)
-})
 </script>
 
 <template>
   <router-view />
-  <div title="返回顶部" class="go-top-wrap" v-show="showGoTop" @click="goTop">
-    <vi-icon name="top" size="30px" cursor="pointer" />
-  </div>
 </template>
 
 <style lang="scss">
-.go-top-wrap {
-  user-select: none;
-  position: fixed;
-  right: 100px;
-  bottom: 100px;
-  border: 1px solid #ddd;
-  background-color: #fff;
-  padding: 10px;
-  cursor: pointer;
-  &:hover { background-color: #ddd; }
-}
 </style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue'
 import HeaderNav from '../components/HeaderNav/index.vue'
 import List from '../components/List/index.vue'
 import Content from '../views/Content/index.vue'
 import FooterInfo from '../views/FooterInfo/index.vue'
 
+
+
+const showGoTop = ref(false)
+
+const handleScroll = () => {
+  const halfHeight = window.innerHeight * 0.5
+  if (window.scrollY >= halfHeight) { showGoTop.value = true } 
+  else { showGoTop.value = false }
+}
+const goTop = () => window.scrollTo({ top: 0 })
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
 </script>
 
 <template>
@@ -15,6 +33,9 @@ import FooterInfo from '../views/FooterInfo/index.vue'
       <FooterInfo />
     </section>
   </main>
+  <div title="返回顶部" class="go-top-wrap" v-show="showGoTop" @click="goTop">
+    <vi-icon name="top" size="30px" cursor="pointer" />
+  </div>
 </template>
 
 <style scoped lang="scss">
@@ -25,5 +46,16 @@ import FooterInfo from '../views/FooterInfo/index.vue'
     flex-direction: column;
     flex: 10;
   }
+}
+.go-top-wrap {
+  user-select: none;
+  position: fixed;
+  right: 100px;
+  bottom: 100px;
+  border: 1px solid #ddd;
+  background-color: #fff;
+  padding: 10px;
+  cursor: pointer;
+  &:hover { background-color: #ddd; }
 }
 </style>
