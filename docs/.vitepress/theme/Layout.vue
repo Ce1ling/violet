f4253442fe39584d07debe3e1bc77912a63868e4
Ce1ling <2134045201@qq.com>
2022-12-09 21:51:15 +0800

    增加：新增首页动画效果

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
+import { nextTick } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import FooterInfo from './components/FooterInfo.vue'
 
 const { Layout } = DefaultTheme
+
+// 首页在加载动画时隐藏滚动条
+nextTick(() => {
+  const app: HTMLElement = document.querySelector('#app')!
+  app.style.overflow = 'hidden'
+  setTimeout(() => app.style.overflow = 'auto', 1000)
+})
 </script>
 
 <template>
