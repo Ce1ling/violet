05c3e3119bb962dd7461435ac6a598196dbbe1b5
Ce1ling <2134045201@qq.com>
2022-12-09 15:00:22 +0800

    完善：完善并完成首页

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { basic } from "../stores/components/basic"
 import { form } from "../stores/components/form"
 import { layout } from "../stores/components/layout"
 
+const author = '<a href="https://github.com/Ce1ling" target="_blank">L1en</a>'
 
 export default {
   title: 'Violet',
@@ -11,16 +12,22 @@ export default {
     // nav: [
     //   { text: 'Guide', link: '', activeMatch: '/guide' },
     // ],
+    // 头部社交图标
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Ce1ling/violet' },
+    ],
     // 侧边栏
     sidebar: [
       { text: '基础组件', items: basic },
       { text: '布局组件', items: layout },
       { text: '表单组件', items: form },
     ],
+    // 右侧锚点标题
+    outlineTitle: '目录',
     // 底部信息。注意：当 sidebar 显示时，footer 则不会显示
-    // footer: {
-    //   message: 'Released under the MIT License.',
-    //   copyright: 'Copyright © 2019-present Evan You'
-    // }
+    footer: {
+      message: `Powered by ❤️ ${author}`,
+      copyright: `Copyright © 2021-${new Date().getFullYear()} ${author}`
+    }
   },
 }
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,24 @@
-# this is Homepage(index)
+---
+layout: home
+hero:
+  name: Violet
+  text: 一个基于 Vue3 的 UI 组件库
+  tagline: 点击 “开始” 按钮，开始预览 Violet 的组件吧！
+  actions:
+    - theme: brand
+      text: 开始
+      link: /components/button.md
+    - theme: alt
+      text: 在 GitHub 上查看
+      link: https://github.com/Ce1ling/violet
+features:
+  - icon: ⚡️
+    title: 开发如 “闪电”
+    details: 开发迅捷，您可以如闪电一般迅速完成网站开发
+  - icon: 🖖
+    title: 好评似 “浪潮”
+    details: 好评率高，恰似浪潮般一波接一波
+  - icon: 🛠️
+    title: 组件比 “天” 阔
+    details: 组件全面，好比天空一样广阔的组件
+---
