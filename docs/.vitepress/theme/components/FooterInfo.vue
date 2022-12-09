5a0126f35a40a6c6e4177f8d5a2694146b9fcff5
Ce1ling <2134045201@qq.com>
2022-12-09 20:40:58 +0800

    增加：封装 doc 的 FooterInfo 组件

---
+++ docs/.vitepress/theme/components/FooterInfo.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { nanoid } from 'nanoid'
+
+const links = [
+  { id: nanoid(), title: 'GitHub 地址', link: 'https:github.com/Ce1ling/violet' },
+  { id: nanoid(), title: '作者 GitHub', link: 'https:github.com/Ce1ling' },
+  { id: nanoid(), title: '更新日志', link: '' },
+  { id: nanoid(), title: '参与贡献', link: 'https:github.com/Ce1ling/violet' },
+]
+</script>
+
+<template>
+<footer class="footer-wrap">
+  <ul class="footer-list">
+    <h1>链接</h1>
+    <li class="footer-item" v-for="item in links" :key="item.id">
+      <a :href="item.link" target="_blank">{{ item.title }}</a>
+    </li>
+  </ul>
+</footer>
+</template>
+
+<style scoped lang="scss">
+.footer-wrap {
+  max-height: var(--footer-height);
+  padding: 50px;
+  background-color: #eee;
+  width: calc(100vw - var(--vp-sidebar-width) - var(--scroll-bar-wh));
+  position: absolute;
+  left: -32px;
+  bottom: 0;
+  z-index: 99;
+  .footer-list {
+    h1 { 
+      font-size: 16px; 
+      margin-bottom: 14px;
+    }
+    .footer-item {
+      margin: 8px 0;
+      a { 
+        color: var(--font-color);
+        &:hover {
+          color: var(--primary-color);
+        }
+      }
+    }
+  }
+}
+</style>
