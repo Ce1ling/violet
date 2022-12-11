6ba3bcb20d40cfaa8a284be2ee1aa9546fe631d4
Ce1ling <2134045201@qq.com>
2022-12-11 18:33:38 +0800

    移除：移除旧的 icon 方案

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,6 @@ import { basic } from "../stores/components/basic"
 import { form } from "../stores/components/form"
 import { layout } from "../stores/components/layout"
 import { navigation } from '../stores/components/navigation'
-import { svgBuilder } from '../../src/plugins/svgBuilder'
 
 const author = '<a href="https://github.com/Ce1ling" target="_blank">L1en</a>'
 
@@ -11,12 +10,6 @@ export default {
   title: 'Violet',
   description: 'A Vue3 UI Framework',
   lang: 'zh-CN',
-  markdown: {
-    config: (md) => {
-      // use more markdown-it plugins!
-      // md.use(require('markdown-it-xxx'))
-    }
-  },
   themeConfig: {
     // 头部导航
     // nav: [
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// @ts-ignore
-import { svgBuilder } from './src/plugins/svgBuilder'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    svgBuilder('./src/assets/icons/')
   ]
 })
