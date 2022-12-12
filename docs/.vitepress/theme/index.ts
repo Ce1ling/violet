c7b682634789c7f21909367d3b9c9a74ca905c36
Ce1ling <2134045201@qq.com>
2022-12-12 14:35:25 +0800

    完善：优化组件注册方式，可自动注册

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,20 +1,15 @@
 import customeLayout from './Layout.vue'
-import { Button, Icon, Layout, Tabs } from '../../../packages/components'
+import * as VioletComponents from '../../../packages/components'
 
 import './styles/var.scss'
 import './styles/global.scss'
 import './styles/custome.scss'
 
-const components = [ 
-  { name: 'vi-button', component: Button },
-  { name: 'vi-icon', component: Icon },
-  { name: 'vi-layout', component: Layout },
-  { name: 'vi-tabs', component: Tabs },
-]
-
 export default {
   Layout: customeLayout,
   enhanceApp({ app }) {
-    components.forEach(item => app.component(item.name, item.component))
+    Object.entries(VioletComponents).forEach(([name, component]) => {
+      app.component(`vi-${name.toLowerCase()}`, component)
+    })
   },
 }
