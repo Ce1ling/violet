50760951815b830f300831775841fa58cb46d748
Ce1ling <2134045201@qq.com>
2022-12-17 16:41:54 +0800

    完善：优化 Tab 存放位置

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -2,7 +2,7 @@ import Button from './Button/index.vue'
 import Layout from './Layout/index.vue'
 import Icon from './Icon/index.vue'
 import Tabs from './Tabs/index.vue'
-import Tab from './Tab/index.vue'
+import Tab from './Tabs/Tab.vue'
 
 export {
   Button,
