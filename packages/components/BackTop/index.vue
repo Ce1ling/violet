0c362296fda2fba50ec185050b463f9790c1b2f0
Ce1ling <2134045201@qq.com>
2023-03-14 14:30:06 +0800

    feat(backtop)：新增 round 圆形

--- packages/components/BackTop/index.vue
+++ packages/components/BackTop/index.vue
@@ -7,21 +7,26 @@ interface Props {
   right?: number
   bottom?: number
   zIndex?: number
+  round?: boolean
 }
 const props = withDefaults(defineProps<Props>(), {
   showHeight: 500,
   hideWidth: -Infinity,
   right: 50,
   bottom: 50,
-  zIndex: 99
+  zIndex: 99,
+  round: false
 })
 
 const show = ref(false)
 
 const customStyle = computed(() => ({ 
   right: props.right + 'px', 
-  bottom: props.bottom + 'px',
-  zIndex: props.zIndex,
+  bottom: props.bottom + 'px', 
+  zIndex: props.zIndex 
+}))
+const classObj = computed(() => ({
+  'is-round': props.round
 }))
 
 const handleScroll = () => show.value = document.body.offsetWidth > props.hideWidth
@@ -34,7 +39,12 @@ onUnmounted(() => window.removeEventListener('scroll', handleScroll))
 </script>
 
 <template>
-  <div class="vi-backtop" v-if="show" @click="backtop" :style="customStyle">
+  <div 
+    class="vi-backtop" 
+    :class="classObj"
+    :style="customStyle"
+    @click="backtop"
+    v-if="show">
     <slot>
       <vi-icon 
         name="Top" 
@@ -52,11 +62,15 @@ onUnmounted(() => window.removeEventListener('scroll', handleScroll))
   position: fixed;
   border: 1px solid var(--vi-color-primary);
   background-color: var(--vi-color-white);
+  box-shadow: 0 0 8px var(--vi-color-gray);
   cursor: pointer;
   &:hover {
     background-color: var(--vi-color-primary);
     .vi-backtop__icon svg { color: #fff; }
   }
   &__icon { display: block; }
+  &.is-round {
+    border-radius: 50%;
+  }
 }
 </style>
