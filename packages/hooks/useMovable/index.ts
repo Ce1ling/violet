9aad4b804754779449aa5bd6f55c1c8a2154962c
Ce1ling <2134045201@qq.com>
2023-03-23 20:03:00 +0800

    fix(dialog)：修复拖拽功能二次拖动回弹 bug

--- packages/hooks/useMovable/index.ts
+++ packages/hooks/useMovable/index.ts
@@ -1,4 +1,4 @@
-import { nextTick, onUnmounted } from 'vue'
+import { nextTick, onBeforeUnmount } from 'vue'
 import type { Ref } from 'vue'
 
 
@@ -8,21 +8,26 @@ type ElRef = Ref<HTMLElement | undefined>
 /** 使一个元素可以被鼠标拖动 */
 export const useMovable = (targetRef: ElRef, moveRef: ElRef, movable: boolean) => {
   const onMouseDown = (e: MouseEvent) => {
+    const targetRect = targetRef.value!.getBoundingClientRect()
     const p = {
-      x: e.clientX,
-      y: e.clientY 
+      x: e.clientX - targetRect.left,
+      y: e.clientY - targetRect.top
     }
+    
     const onMouseMove = (e: MouseEvent) => {
+      const targetStyle = window.getComputedStyle(targetRef.value as Element)
       const r = {
-        x: e.clientX - p.x,
-        y: e.clientY - p.y
+        x: e.clientX - p.x - parseInt(targetStyle.marginLeft),
+        y: e.clientY - p.y - parseInt(targetStyle.marginTop)
       }
+      
       targetRef.value!.style.transform = `translate(${r.x}px, ${r.y}px)`
     }
     const onMouseUp = () => {
       document.removeEventListener('mousemove', onMouseMove)
       document.removeEventListener('mouseup', onMouseUp)
     }
+
     document.addEventListener('mousemove', onMouseMove)
     document.addEventListener('mouseup', onMouseUp)
   }
@@ -30,5 +35,5 @@ export const useMovable = (targetRef: ElRef, moveRef: ElRef, movable: boolean) =
   nextTick(() => {
     if (movable) { moveRef.value?.addEventListener('mousedown', onMouseDown) }
   })
-  onUnmounted(() => moveRef.value?.removeEventListener('mousedown', onMouseDown))
+  onBeforeUnmount(() => moveRef.value?.removeEventListener('mousedown', onMouseDown))
 }
