0245171478d34a6c7e179114bc4aeb2146286838
Ce1ling <2134045201@qq.com>
2023-03-27 21:31:32 +0800

    feat(mask)：新增 mask 组件

---
+++ packages/components/Mask/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+interface Props {
+  visible: boolean
+}
+defineProps<Props>()
+
+</script>
+
+<template>
+  <div class="vi-mask" v-show="visible">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.vi-mask {
+  background-color: var(--vi-color-mask-black);
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 9999;
+}
+</style>
