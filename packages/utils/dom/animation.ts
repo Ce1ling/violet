7bfd715d76699b2359a9f8374ae39c0e115190ec
Ce1ling <2134045201@qq.com>
2023-03-30 16:49:37 +0800

    feat(utils)：新增根据 CSS 变量获取属性值的工具

---
+++ packages/utils/dom/animation.ts
@@ -0,0 +1,18 @@
+import { computed } from 'vue'
+
+interface AnimationDuration {
+  (target: HTMLElement, variable: string, unit?: number): number
+}
+
+/**
+ * 根据获取动画执行时间
+ * @param {HTMLElement} target 目标元素
+ * @param {String} variable 需获取的 CSS 变量
+ * @param {Number} unit 可选。将结果与此参数做"乘法"，以便移动小数点获得各种单位
+ * @return {Number} 返回结果
+ */
+export const getAnimationDurationByVariable: AnimationDuration = (target, variable, unit) => computed(() => {
+  const d = window.getComputedStyle(target).getPropertyValue(variable)
+  const r = window.parseFloat(d)
+  return unit ? r * unit : r
+}).value
