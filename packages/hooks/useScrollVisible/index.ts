7ec781d09e8d83a5f384bb03ca84f4842c85db7b
Ce1ling <2134045201@qq.com>
2023-04-09 20:36:48 +0800

    refactor(hooks)：优化 useScrollVisible，在 hook 侦听

--- packages/hooks/useScrollVisible/index.ts
+++ packages/hooks/useScrollVisible/index.ts
@@ -1,34 +1,45 @@
+import { onScopeDispose, watch } from 'vue'
 import { getScrollWidth } from '../../utils/dom/scroll'
+import { addClass, removeClass, overflow } from '../../utils/dom/style'
 
+import type { Ref } from 'vue'
 
 interface UseScrollVisible {
-  (t: HTMLElement, cls?: string): { 
-    show: () => void, 
-    hide: () => void 
-  }
+  (trigger: Ref<boolean>, target: HTMLElement, cls?: string): void
 }
+
 /** 
  * 隐藏/显示 网页的滚动条
+ * @param {Ref<boolean>} trigger 用于触发 隐藏/显示。它必须是 Ref！为 `true` 时，隐藏滚动条，`false` 显示。
  * @param {HTMLElement} target 需操作的元素
  * @param {String} cls 隐藏滚动条的类名，不传则手动操作 DOM(此参数非常推荐传递)
- * @return {object} { show, hide }，调用 show 显示滚动条，hide 隐藏。
  **/
-export const useScrollVisible: UseScrollVisible = (target, cls) => {
-  const width = getScrollWidth('px')
-  const classList = target.classList
-  const style = target.style
-
+export const useScrollVisible: UseScrollVisible = (trigger, target, cls) => {
   const show = () => {
-    cls ? classList.remove(cls) : style.overflow = ''
-    style.width = ''
+    if (cls) {
+      removeClass(target, cls)
+      return
+    }
+    overflow(target, '')
+    target.style.width = ''
   }
   const hide = () => {
-    cls ? classList.add(cls) : style.overflow = 'hidden'
-    style.width = `calc(100% - ${width})`
+    if (cls) {
+      addClass(target, cls)
+      return
+    }
+    overflow(target, 'hidden')
+    target.style.width = `calc(100% - ${getScrollWidth('px')})`
   }
 
-  return {
-    show,
-    hide
-  }
+  watch(trigger, (val) => {
+    if (val) {
+      hide()
+    } else {
+      show()
+    }
+  })
+
+  // 在当前 Hook 作用域停止后还原滚动条
+  onScopeDispose(show)
 }
