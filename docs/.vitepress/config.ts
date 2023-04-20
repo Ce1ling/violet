9f89345c8e4d4a6275a5351a90b72bb867e420c8
Ce1ling <2134045201@qq.com>
2023-04-20 16:02:04 +0800

    docs：规范 favicon 文件

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -12,7 +12,7 @@ export default {
   description: 'A Vue3 UI Framework',
   lang: 'zh-CN',
   head: [
-    ['link', { rel: 'icon', href: '/violet-logo.svg',  type: 'image/svg+xm' }]
+    ['link', { rel: 'icon', href: '/violet-logo.svg',  type: 'image/svg+xml' }]
   ],
   markdown: {
     theme: {
