61892232dfcb04dd3bd266588853937af6e8a4c2
Ce1ling <2134045201@qq.com>
2023-04-29 22:17:10 +0800

    docs: 将 docs utils 移动到 docs 并优化 format 函数

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,7 @@
 import Layout from './Layout.vue'
 import NotFound from './NotFound.vue'
 import * as VioletComponents from '../../../packages/components'
-import { format } from '../../../packages/utils/compNameFormat'
+import { format } from '../../utils/format'
 
 import './styles/var.scss'
 import './styles/global.scss'
@@ -13,7 +13,7 @@ export default {
   NotFound,
   enhanceApp({ app }) {
     Object.entries(VioletComponents).forEach(([name, component]) => {
-      app.component(format(name, /[A-Z]/g), component)
+      app.component(format('vi', name, /[A-Z]/g), component)
     })
   },
 }
