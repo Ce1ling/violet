9a2de72e71d5cd11804d066c169f5535cb0c6faf
Ce1ling <2134045201@qq.com>
2023-05-02 13:27:17 +0800

    docs: 注册文档组件，增加文档示例样式

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,8 @@
 import Layout from './Layout.vue'
 import NotFound from './NotFound.vue'
+import { format } from '../utils/format'
 import * as VioletComponents from '../../../packages/components'
-import { format } from '../../utils/format'
+import * as DocsComponents from '../vitepress/index'
 
 import './styles/var.scss'
 import './styles/global.scss'
@@ -12,8 +13,14 @@ export default {
   Layout,
   NotFound,
   enhanceApp({ app }) {
+    /** 注册 Violet UI 组件 */
     Object.entries(VioletComponents).forEach(([name, component]) => {
       app.component(format('vi', name, /[A-Z]/g), component)
+    })
+
+    /** 注册 docs 组件 */
+    Object.entries(DocsComponents).forEach(([name, component]) => {
+      app.component(name, component)
     })
   },
 }
