18237cda619f6f82f1d81b57584abe8dc0a68ddb
Ce1ling <2134045201@qq.com>
2023-05-03 20:20:03 +0800

    chore(hooks): [useScrollVisible] 新增错误处理

--- packages/hooks/useScrollVisible/index.ts
+++ packages/hooks/useScrollVisible/index.ts
@@ -1,11 +1,11 @@
-import { onScopeDispose, watch } from 'vue'
+import { onScopeDispose, watch, isRef } from 'vue'
 import { getScrollWidth } from '../../utils/dom/scroll'
 import { addClass, removeClass, overflow } from '../../utils/dom/style'
 import { useTimeout } from './../useTimeout/index'
 
 import type { Ref } from 'vue'
 
-interface UseScrollVisible {
+export interface UseScrollVisible {
   (
     trigger: Ref<boolean>, 
     target: HTMLElement, 
@@ -15,11 +15,11 @@ interface UseScrollVisible {
 }
 
 /** 
- * 隐藏/显示 网页的滚动条
- * @param {Ref<boolean>} trigger 用于触发 隐藏/显示。它必须是 Ref！为 `true` 时，隐藏滚动条，`false` 显示。
- * @param {HTMLElement} target 需操作的元素
- * @param {String} cls 隐藏滚动条的类名，不传则手动操作 DOM(此参数非常推荐传递)
- * @param {Number} timeout 延迟显示滚动条(ms)
+ * 隐藏/显示 指定元素的滚动条
+ * @param {Ref<boolean>} trigger 用于触发. 必须是 Vue 的 ref, 当它发生改变时, 会触发 显示/隐藏(`true` 隐藏, `false` 显示)
+ * @param {HTMLElement} target 待操作的元素
+ * @param {String} cls 可选. 隐藏滚动条的类名, 不传则手动操作 DOM(此参数非常推荐传递)
+ * @param {Number} timeout 可选. 延迟显示滚动条(ms)
  **/
 export const useScrollVisible: UseScrollVisible = (trigger, target, cls, timeout) => {
   const show = () => {
@@ -37,6 +37,10 @@ export const useScrollVisible: UseScrollVisible = (trigger, target, cls, timeout
     }
     overflow(target, 'hidden')
     target.style.width = `calc(100% - ${getScrollWidth('px')})`
+  }
+
+  if (!isRef(trigger)) {
+    throw new Error('`trigger` 必须是一个 Vue 的 `ref` !')
   }
 
   watch(trigger, (val) => {
