ec37b7edaf10e52451e57ed43906a595c6c53be7
Ce1ling <2134045201@qq.com>
2023-05-04 22:04:26 +0800

    fix(hooks): [useScrollVisible] 修复隐藏时滚动条抖动的问题，并优化 hook 用法

--- packages/hooks/useScrollVisible/index.ts
+++ packages/hooks/useScrollVisible/index.ts
@@ -1,6 +1,6 @@
 import { onScopeDispose, watch, isRef } from 'vue'
 import { getScrollWidth } from '../../utils/dom/scroll'
-import { addClass, removeClass, overflow } from '../../utils/dom/style'
+import { addClass, removeClass } from '../../utils/dom/style'
 import { useTimeout } from './../useTimeout/index'
 
 import type { Ref } from 'vue'
@@ -9,52 +9,47 @@ export interface UseScrollVisible {
   (
     trigger: Ref<boolean>, 
     target: HTMLElement, 
-    cls?: string,
-    timeout?: number
+    timeout?: number,
+    cls?: string
   ): void
 }
+
+const violetScrollClass = 'vi-scroll-hide'
 
 /** 
  * 隐藏/显示 指定元素的滚动条
  * @param {Ref<boolean>} trigger 用于触发. 必须是 Vue 的 ref, 当它发生改变时, 会触发 显示/隐藏(`true` 隐藏, `false` 显示)
  * @param {HTMLElement} target 待操作的元素
- * @param {String} cls 可选. 隐藏滚动条的类名, 不传则手动操作 DOM(此参数非常推荐传递)
  * @param {Number} timeout 可选. 延迟显示滚动条(ms)
+ * @param {String} cls 可选. 隐藏滚动条的类名
  **/
-export const useScrollVisible: UseScrollVisible = (trigger, target, cls, timeout) => {
-  const show = () => {
-    if (cls) {
-      removeClass(target, cls)
-      return
-    }
-    overflow(target, '')
-    target.style.width = ''
-  }
-  const hide = () => {
-    if (cls) {
-      addClass(target, cls)
-      return
-    }
-    overflow(target, 'hidden')
-    target.style.width = `calc(100% - ${getScrollWidth('px')})`
-  }
-
+export const useScrollVisible: UseScrollVisible = (trigger, target, timeout, cls = violetScrollClass) => {
   if (!isRef(trigger)) {
     throw new Error('`trigger` 必须是一个 Vue 的 `ref` !')
   }
 
-  watch(trigger, (val) => {
+  watch(trigger, val => {
     if (val) {
-      hide()
+      add()
       return
     } 
+
     if (timeout) {
-      useTimeout(show, timeout)
+      useTimeout(remove, timeout)
       return
     }
-    show()
+    remove()
   })
 
-  // 在当前 Hook 作用域停止后还原滚动条
-  onScopeDispose(show)
+  const add = () => {
+    target.style.width = `calc(100% - ${getScrollWidth('px')})`
+    addClass(target, cls)
+  }
+  
+  const remove = () => {
+    target.style.width = ''
+    removeClass(target, cls)
+  }
+  
+  onScopeDispose(remove)
 }
