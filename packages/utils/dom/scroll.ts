595cc5d4bca76374267d36fa9c9983ab3b8689a9
Ce1ling <2134045201@qq.com>
2023-05-10 15:52:23 +0800

    feat(utils): [hasScrollBar] 新增判断是否存在滚动条工具

--- packages/utils/dom/scroll.ts
+++ packages/utils/dom/scroll.ts
@@ -24,3 +24,28 @@ export const getScrollWidth = (unit?: string): string | number => {
 
   return unit ? scrollWidth + unit : scrollWidth
 }
+
+
+interface HasScrollBar {
+  (direction?: 'x' | 'y' | 'all'): Boolean | [Boolean, Boolean]
+}
+/**
+ * 是否存在滚动条
+ * @param {Direction} d 判断的方位。`'x'` 为水平滚动条, `'y'` 为垂直滚动条, `'all'` 为所有滚动条。默认为 `'y'`。
+ * @returns 默认返回 `boolean`。`true` 代表有, `false` 代表没有。若 `d` 为 `'all'` 则返回一个二元组, 元素1代表 `'x'`, 元素2 代表 `'y'`。
+ */
+export const hasScrollBar: HasScrollBar = (d = 'y') => {
+  const body = document.body || document.documentElement
+  const x = window.innerWidth < body.scrollWidth
+  const y = window.innerHeight < body.scrollHeight
+
+  if (d === 'x') {
+    return x
+  } else if (d === 'y') {
+    return y
+  } else if (d === 'all') {
+    return [x, y]
+  } else {
+    throw new Error('`参数` 只能为: `x` | `y` | `all` 中的一个')
+  }
+}
