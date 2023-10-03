a8609a31685451d6b67db8fac4c226261aa82eec
Ce1ling <2134045201@qq.com>
2023-10-03 18:34:16 +0800

    refactor(docs): 优化文档样式划分

--- packages/hooks/useScrollVisible/index.ts
+++ packages/hooks/useScrollVisible/index.ts
@@ -7,8 +7,8 @@ import type { Ref } from 'vue'
 
 export interface UseScrollVisible {
   (
-    trigger: Ref<boolean>, 
-    target: HTMLElement, 
+    trigger: Ref<boolean>,
+    target: HTMLElement,
     timeout?: number,
     cls?: string
   ): void
@@ -16,14 +16,19 @@ export interface UseScrollVisible {
 
 const violetScrollClass = 'vi-scroll-hide'
 
-/** 
+/**
  * 隐藏/显示 指定元素的滚动条
  * @param {Ref<boolean>} trigger 用于触发. 必须是 Vue 的 ref, 当它发生改变时, 会触发 显示/隐藏(`true` 隐藏, `false` 显示)
  * @param {HTMLElement} target 待操作的元素
  * @param {Number} timeout 可选. 延迟显示滚动条(ms)
  * @param {String} cls 可选. 隐藏滚动条的类名
  **/
-export const useScrollVisible: UseScrollVisible = (trigger, target, timeout, cls = violetScrollClass) => {
+export const useScrollVisible: UseScrollVisible = (
+  trigger,
+  target,
+  timeout,
+  cls = violetScrollClass
+) => {
   if (!isRef(trigger)) {
     throw new Error('`trigger` 必须是一个 Vue 的 `ref` !')
   }
@@ -35,7 +40,7 @@ export const useScrollVisible: UseScrollVisible = (trigger, target, timeout, cls
     if (val) {
       add()
       return
-    } 
+    }
 
     if (timeout) {
       useTimeout(remove, timeout)
@@ -44,15 +49,26 @@ export const useScrollVisible: UseScrollVisible = (trigger, target, timeout, cls
     remove()
   })
 
-  const add = () => {
-    target.style.width = `calc(100% - ${getScrollWidth('px')})`
-    addClass(target, cls)
+  // 处理文档顶部抖动
+  const handleDocHeader = (isHidden: boolean, width?: string) => {
+    const header = document.querySelector('#app .VPNav') as HTMLElement
+    header.style.width = isHidden ? width! : ''
   }
-  
+
+  const add = () => {
+    const bodyWidth = `calc(100% - ${getScrollWidth('px')})`
+    target.style.width = bodyWidth
+    target.style.overflow = 'hidden'
+
+    handleDocHeader(true, bodyWidth)
+  }
+
   const remove = () => {
     target.style.width = ''
-    removeClass(target, cls)
+    target.style.overflow = ''
+
+    handleDocHeader(false)
   }
-  
+
   onScopeDispose(remove)
 }
